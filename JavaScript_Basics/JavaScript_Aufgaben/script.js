0f16331c99635127d351d3069772f3fc4e13e797
andrejbsvc <andrej.barosevcic@gmail.com>
2023-11-19 14:28:18 +0100

    Finished Übung JavaScript and changed the location of my folder

---
+++ JavaScript_Basics/JavaScript_Aufgaben/script.js
@@ -0,0 +1,88 @@
+//Aufgaben von Herr Riedmann
+
+console.log("Hallo");
+console.log("Tolle Sache");
+
+
+//Variablen
+
+let a = 3;
+let b = a + 3;
+let c = 10 - 3;
+
+//Berechne folgendes und gib das Ergebnis über console.log aus
+
+
+//a + b + c
+
+let result = a + b + c;
+console.log (result);
+//Ergebnis = 25
+//Ergbnis mit a = 3, = 21
+//Ergebnis mit a = 3, und b = a + 3, = 19
+//Ergebnis mit a = 3, und b = a + 3, und c = 10 - 3, = 16
+
+
+
+
+
+
+//(a + b) / c
+
+result = (a + b) / c;
+console.log (result);
+//Ergebnis = 1,5
+//Ergebnis mit a = 3, = 1,1
+//Ergebnis mit a = 3, und b = a + 3, = 0,9
+//Ergebnis mit a = 3, und b = a + 3, und c = 10 - 3, = gerundet 1,29
+
+
+
+
+
+//c - a + b
+
+result = c - a + b;
+console.log (result);
+//Ergebnis = 11
+//Ergebnis mit a = 3, = 15
+//Ergebnis mit a = 3, und b = a + 3, = 13
+//Ergebnis mit a = 3, und b = a + 3, und c = 10 - 3, = 10
+
+
+
+
+
+
+//a + b + c * 3
+
+result = a + b + c * 3;
+console.log (result);
+//Ergebnis = 45
+//Ergebnis mit a = 3, = 41
+//Ergebnis mit a = 3, und b = a + 3, = 39
+//Ergebnis mit a = 3, und b = a + 3, und c = 10 - 3, = 30
+
+
+
+
+//(a + b +c) / 4
+
+result = (a + b + c) / 4
+console.log(result);
+//Ergebnis = 6,25
+//Ergebnis mit a = 3, = 5,25
+//Ergebnis mit a = 3, und b = a + 3, = 4,75
+//Ergebnis mit a = 3, und b = a + 3, und c = 10 - 3, = 4
+
+
+
+
+
+
+
+//Ändere nun die Variablen auf folgende Werte 
+//a = 3
+//b = a + 3
+//c = c - 3
+
