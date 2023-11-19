c52c2778bc9a79db9738d100d1171ad86e80b291
andrejbsvc <andrej.barosevcic@gmail.com>
2023-11-19 14:03:26 +0100

    Finished JavaScript Video

---
+++ JavaScript_Basics/script.js
@@ -0,0 +1,25 @@
+console.log("Hallo Du");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = a + b;
+
+
+console.log("A hat den Wert " +  a);
+
+
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+// *, /, +, -
+
+c = b - a;
+
+console.log("b - a = " + c);
+
+
+
