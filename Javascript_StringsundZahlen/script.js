d72ce881032bb29adb7db6ef9e187407df6e3e49
andrejbsvc <andrej.barosevcic@gmail.com>
2023-11-27 19:23:16 +0100

    Finished Video Javascript 2 and finished Übung js 2

---
+++ Javascript_StringsundZahlen/script.js
@@ -0,0 +1,31 @@
+let a = 7.4;
+let b = 7.5;
+
+let ab = a + b;
+
+result = a + b;
+console.log(result);
+
+// Texte STRINGS
+
+let name ="Andrej"
+
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+//Übung JavaScript 2
+
+let c = 17.4
+let d = 3.9
+
+let cb = c / d;
+
+result = c / d;
+console.log("The result of " + c + " divided by " + d + " is " + result); 
+
+
+let firstname = "Andrej"
+let lastname = "Barosevcic"
+
+console.log("Ich heiße "  + firstname +  lastname );
