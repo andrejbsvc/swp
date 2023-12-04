5fb0487671f38b4b2190fe656d39c1726356b98b
andrejbsvc <andrej.barosevcic@gmail.com>
2023-12-04 12:19:15 +0100

    finished Übung JavaScript 04 Loops

---
+++ Schleifen/script.js
@@ -0,0 +1,51 @@
+
+
+//Schleife
+
+for (let count = 0; count < 10; count++) {
+    console.log("Andrej")
+}
+
+
+
+//Zähle von 7 bis 45
+
+for (let count = 7; count < 46; count++) {
+    console.log(count)
+}
+console.log("Fertig")
+
+
+
+
+//Zähle von 9 bis 99
+
+for (let count = 9; count < 99; count++) {
+    console.log(count)
+}
+console.log("Fertig")
+
+
+//Zähle Rückwärts von 10 bis 0
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count)
+}
+console.log("Fertig")
+
+
+//Zähle rückwärts von 1234 bis 1207
+
+for (let count = 1234; count >= 1207; count--) {
+    console.log(count)
+}
+console.log("Fertig")
+
+
+
+//Gib deinen Namen genau 8 mal aus
+
+for (let count = 0; count <= 10; count++) {
+    console.log(count)
+}
+console.log("Andrej")
