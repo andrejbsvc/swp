fa93d36c0727a6dd5666e794265aab868b00b83c
andrejbsvc <andrej.barosevcic@gmail.com>
2023-12-04 11:10:28 +0100

    Finished Übung JavaScript 03

---
+++ JavaScript_AufgabeVariablen/script.js
@@ -0,0 +1,51 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+//Erstelle eine Variable apple mit dem Wert "Apple"
+
+let banana = "Banane";
+let apple = "Apple";
+
+//Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+//Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+//Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+//Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+let appleWeightSingle = 0.34;
+let bananaWeightSingle = 0.22;
+
+//Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+
+
+//Preis von 8 Äpfel
+
+console.log(applePricePerKilo / 8)
+//Price one apple 0.42 Cent
+
+console.log(0.42 * 8)
+//8 Äpfel kosten 3.36 Euro
+
+
+
+//Preis von 17 Bananen
+
+console.log(bananaPricePerKilo / 17)
+//Price one banana 0.12 Cent
+console.log(0.12 * 17)
+//17 Bananen kosten 2.04 Euro
+
+
+
+
+//Preis von 1 Tonne Äpfel
+console.log(applePricePerKilo * 1000)
+//1 Tonne Äpfel kostet 3430 Euro
+
+
+
+
+//Preis von 1 Tonne Bananen
+console.log(bananaPricePerKilo * 1000)
+//1 Tonne Bananen kosten 2140 Euro
