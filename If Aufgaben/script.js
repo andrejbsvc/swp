6b9116b551e630bd846e00250ededb65edc321fb
andrejbsvc <andrej.barosevcic@gmail.com>
2023-12-11 11:31:54 +0100

    finished the 2 Tasks of if Bedingungen

---
+++ If Aufgaben/script.js
@@ -0,0 +1,62 @@
+let variable = 17;
+
+
+//Wenn die Zahl kleiner ist als 20 gib aus "Mini"
+
+if (variable < 20) {
+    console.log(variable + " ist gleich Mini")
+}
+
+
+
+//Wenn die Zahl zwischen 20 und 50 ist gib aus "Medium"
+
+if (variable >= 20 && variable <= 50) {
+    console.log(variable + " ist gleich Medium")
+} else {
+    console.log(variable + " ist nicht Medium")
+}
+
+
+//Wen die Zahl größer als 50 ist gib aus "Large"
+
+if (variable > 50) {
+    console.log(variable + " ist gleich Large")
+} else {
+    console.log(variable + " ist nicht Large")
+}
+
+
+
+
+//Aufgabe 2
+
+//Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+
+if (variable <= 7) {
+    console.log(variable + " ist gleich Mini")
+
+} else {
+    console.log(variable + " ist größer als 7")
+}
+
+//Wenn die Zahl zwischen 7 und 90 ist gib aus "Medium"
+
+if (variable >= 7 && variable <= 90 ) {
+    console.log(variable + " ist gleich Medium")
+
+} else {
+    console.log(variable + " ist nicht Medium")
+
+}
+
+
+//Wenn die Zahl größer als 90 ist gib aus "Large"
+
+if (variable >= 90) {
+    console.log(variable + " ist gleich Large")
+
+} else {
+    console.log(variable + " ist nicht Large")
+    
+}
