150fe3b620b3b3a33f8069411cae79851c225482
andrejbsvc <andrej.barosevcic@gmail.com>
2024-04-22 11:20:41 +0200

    abgabe test

---
+++ Test3/test3.js
@@ -0,0 +1,32 @@
+const data = "Hans geht heute schwimmen das ist gut";
+// Drehe die Wörter um
+// Output
+// schwimmen heute geht Hans
+
+const numbers = "8,2,1,4,6,1,8,7";
+// finde die größte Zahl und gib an wie oft sie vorkommt
+// Output: 8 2
+
+let highestNumber = 8;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers <= highestNumber){
+        console.log(numbers)
+        
+    }
+
+
+}
+
+const temperatures = "-1,5,51,-10,-11,2,90,-8,-44";
+
+
+    
+if(temperatures >= 50 && -30);
+    console.log("Ausreißer");
+// Berechne den Mittelwert der Temperaturen
+// Werte die größer gleich 50 und kleiner gleich -30 sind sind Ausreißer und dürfen nicht berücksichtigt werden
+
+
+
+
