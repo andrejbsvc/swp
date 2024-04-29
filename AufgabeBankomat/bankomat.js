acd02eba885d6e8d45ec0cf4d53d0565e210ec45
andrejbsvc <andrej.barosevcic@gmail.com>
2024-04-29 11:09:25 +0200

    finished Aufgabe Bankomat

--- AufgabeBankomat/bankomat.js
+++ AufgabeBankomat/bankomat.js
@@ -1,47 +1,69 @@
-
-
-import { link } from "fs";
+import { resolve } from "path";
 import { createInterface } from "readline";
-
-const readline = createInterface ( {
+ 
+const readline = createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-
-const readlineAsync = () => {
+ 
+const readLineAsync = () => {
     return new Promise((resolve) => {
         readline.question("", (userRes) => {
-        resolve(userRes);
-        readline.close();
+            resolve(userRes);
+            //readline.close();
         });
     });
 };
-
+ 
 let balance = 0;
-
-function einzahlen(amount) {
-    balance += amount;
-    console.log("Das Einzahlen war erfolgreich, Ihr Kontostand beträgt jetzt " + amount);
-}
-
-function abheben(amount) {
-    if(amount <= balance) {
-        console.log("Abheben war erfolgreich, Ihr Kontostand beträgt jetzt " + amount);
-
-    } else {
-        console.log("Sie haben nicht genug Geld auf dem Konto zum abheben!");
+mainmenu();
+ 
+async function mainmenu() {
+    console.log("Selektieren sie die gewünschte Funktion");
+    console.log("1. Einzahlen");
+    console.log("2. Abheben");
+    console.log("3. Kontostand");
+    console.log("4. Beenden");
+    let selection = await readLineAsync();
+    switch(selection) {
+        case "1":
+            await Einzahlen();
+            break;
+        case "2":
+            await Abheben();
+            break;
+        case "3":
+            Kontostand();
+            break;
+        case "4":
+            Beenden();
+            break;
     }
 }
-
-function kontostandAnzeigen() {
-    console.log("Aktueller Kontostand " + amount);
-    
+ 
+async function Einzahlen() {
+    console.log("Wie viel möchten sie einzahlen?");
+    let amount = await readLineAsync();
+    balance += parseInt(amount);;
+    console.log(`Ihr neuer Kontostand beträgt ${balance}`);
+    mainmenu();
 }
-
-function bankautomat() {
-    while (true) {
-        
-    }
+ 
+async function Abheben() {
+    console.log("Wie viel möchten sie abheben?");
+    let amount = await readLineAsync();
+    balance -= parseInt(amount);
+    console.log(`Ihr neuer Kontostand beträgt ${balance}`);
+    mainmenu();
 }
-
-    
+ 
+function Kontostand() {
+    console.log(`Ihr Kontostand beträgt ${balance}`);
+    mainmenu();
+}
+ 
+function Beenden() {
+    console.log("Machine is stopping...");
+    readline.close();
+    process.exit();
+}
